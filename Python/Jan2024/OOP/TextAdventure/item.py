7bcfa2c4e3954f903506a424f9190fbd02fc3c2d
Matthattan <roshidridwan@gmail.com>
2024-01-17 16:07:31 +0000

    Text Adventure 3

--- Python/Jan2024/OOP/TextAdventure/item.py
+++ Python/Jan2024/OOP/TextAdventure/item.py
@@ -8,7 +8,7 @@ class Item:
 # create a weapon for characters to use
 class Weapon(Item):
     def __init__(self, name: str, weapon_type: str, damage: int, value: int, desc: str):
-        super.__init__(name=name, weapon_type=weapon_type, value=value, desc=desc)
+        super().__init__(name=name, weapon_type=weapon_type, value=value, desc=desc)
 
         self.damage = damage
     
@@ -18,7 +18,7 @@ class Weapon(Item):
 
 class Medicine(Item):
     def __init__(self, name: str, weapon_type: str, value: int, desc: str, hpRecover):
-        super.__init__(name=name, weapon_type=weapon_type, value=value, desc=desc)
+        super().__init__(name=name, weapon_type=weapon_type, value=value, desc=desc)
 
         self.hpRecover = hpRecover
     
@@ -26,12 +26,12 @@ class Medicine(Item):
         return f"{self.name}: {self.weapon_type} Medicine that heals {self.hpRecover} health and is worth {self.value} gold. {self.desc}"
 
 # Medicines
-smallMed = Medicine(name="Small Med", weapon_type="support", hpRecover=5, desc="Those over-the-counter presecriptions that have been modified to regain health.")
-mediumMed = Medicine(name="Medium Med", weapon_type="support", hpRecover=20, desc="For those times when you aren't that guy.")
-largeMed = Medicine(name="Large Med", weapon_type="support", hpRecover=50, desc="Tis but a scratch")
+smallMed = Medicine(name="Small Med", weapon_type="support", hpRecover=5, value=10,desc="Those over-the-counter presecriptions that have been modified to regain health.")
+mediumMed = Medicine(name="Medium Med", weapon_type="support", hpRecover=20, value=20, desc="For those times when you aren't that guy.")
+largeMed = Medicine(name="Large Med", weapon_type="support", hpRecover=50, value=40, desc="Tis but a scratch")
 
 # Weapons
-nuke = Weapon(name="An Actual Nuke", weapon_type="ranged", damage=1000, value=200, desc="My best friend :>.")
-iron_sword = Weapon(name="Iron Sword", weapon_type="melee", damage=15, value=10, desc="The standard for medieval battles.")
-short_bow = Weapon(name="Short Bow", weapon_type="ranged", damage=5, value=8, desc="For when you don't like social contact.")
+nuke = Weapon(name="An Actual Nuke", weapon_type="ranged", damage=1000, value=999, desc="My best friend :>.")
+iron_sword = Weapon(name="Iron Sword", weapon_type="melee", damage=15, value=50, desc="The standard for medieval battles.")
+short_bow = Weapon(name="Short Bow", weapon_type="ranged", damage=5, value=20, desc="For when you don't like social contact.")
 fists = Weapon(name="Fists", weapon_type="melee", damage=2, value=0, desc="For getting all personal.")
--- Python/Jan2024/OOP/TextAdventure/main.py
+++ Python/Jan2024/OOP/TextAdventure/main.py
@@ -3,62 +3,76 @@ import random
 
 # import character class
 from character import Hero, Enemy
-from weapon import short_bow, iron_sword, nuke
+from item import short_bow, iron_sword, nuke, smallMed, mediumMed, largeMed
 
 # create instances from character
-hero = Hero(name="hero", health=100)
+hero = Hero(name="hero", health=100, inventory=[smallMed, smallMed, short_bow])
 hero.equip(iron_sword)
 enemy = Enemy(name="Enemy", health=100, weapon=short_bow)
 
 print(f"{enemy.name} was encountered!")
 
-# Player Idle
-while True:
-    action = input("What would you like to do \n 1 - Encounter \n 2 - Check Weapon \n 3 - Quit \n")
+class Game:
+    def __init__(self):
 
-    # Encounter
-    if action == "1":
+        # Player Idle
         while True:
-            # get odds
-            encounterRate = random.randint(1,128)
+            action = input("What would you like to do \n 1 - Encounter \n 2 - Check Inventory \n 3 - Check Weapon \n 4 - Quit \n")
 
-            if encounterRate == random.randint(1,128):
-                print(f"You encountered {enemy.name}!")
+            # Encounter
+            if action == "1":
 
-                enemy.health = enemy.health_max
-                enemy.health_bar.update()
+                # get odds
+                encounterRate = random.randint(1,2)
+                nukeRate = random.randint(1,5)
 
-                hero.health_bar.draw()
-                enemy.health_bar.draw()
+                if encounterRate == random.randint(1,5):
+                    print(f"You encountered {enemy.name}!")
 
-                while enemy.health > 0 and hero.health > 0:
-
-                    input()
-
-                    hero.attack(enemy)
-                    enemy.attack(hero)
+                    enemy.health = enemy.health_max
+                    enemy.health_bar.update()
 
                     hero.health_bar.draw()
                     enemy.health_bar.draw()
 
-                # check who won
-                if enemy.health <= 0:
-                    print(f"{enemy.name} was defeated!")
-                    break
+                    while enemy.health > 0 and hero.health > 0:
 
-                elif hero.health <= 0:
-                    print(f"{hero.name} was defeated!")
-                    break
+                        input()
 
-            elif encounterRate == random.randint(1,256):
-                print(f"{hero.name} picked up a {nuke.name}")
-                hero.equip(nuke)
-                break
+                        hero.attack(enemy)
+                        enemy.attack(hero)
 
-    # check weapon            
-    elif action == "2":
-        print(hero.weapon)
-    
-    # quit game
-    elif action == "3":
-        quit()
+                        hero.health_bar.draw()
+                        enemy.health_bar.draw()
+
+                    # check who won
+                    if enemy.health <= 0:
+                        print(f"{enemy.name} was defeated!")
+
+                    elif hero.health <= 0:
+                        print(f"{hero.name} was defeated!")
+
+                        hero.health = hero.health_max
+                        hero.health_bar.update()
+
+                elif nukeRate == random.randint(1,10):
+                    print(f"{hero.name} picked up a {nuke.name}")
+                    hero.equip(nuke)
+
+                else:
+                    print("Nothing Found!")
+
+            # access inventory
+            elif action == "2":
+                hero.viewInv()
+
+            # check weapon            
+            elif action == "3":
+                print(hero.weapon)
+            
+            # quit game
+            elif action == "4":
+                quit()
+
+if __name__ == "__main__":
+    Game()
